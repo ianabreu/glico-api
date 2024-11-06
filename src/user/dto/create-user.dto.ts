e152ddc97f411868a0885306db6824ccc23869b1
Ian Abreu <ianabreudev@gmail.com>
2024-11-05 22:24:57 -0300

    config Global Pipes and validation ofcreateUserDTO

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,6 +1,14 @@
 import { User } from '@prisma/client';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto implements Omit<User, 'id'> {
+  @IsEmail()
+  @IsString()
+  @IsNotEmpty()
   email: string;
+
+  @MinLength(6)
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
